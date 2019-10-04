20205ac65a9fe9af95a1fab2f08a140a683dd207
Joe Denson <joedenson79@gmail.com>
2019-10-04 10:51:37 -0700

    mvp complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import Navbar from './Navbar';
 
-function App() {
+class App extends React.Component {
+constructor(){
+  super();
+  this.state = {
+players: []
+  }
+}
+
+
+componentDidMount(){
+  axios.get('http://localhost:5000/api/players')
+  .then(res=>{
+    console.log(res.data)
+    this.setState({
+      players: res.data
+    })
+  })
+  .catch(err => console.log(err));
+}
+
+
+
+
+render(){
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      
+      <div className='players'>
+      <h2 className='top-100'>Top 100 Players:</h2>
+      {this.state.players.map(player=> (
+        <div key={player.id} player={player.name} className='player'>{player.name}</div>
+      ))}
+      
+      </div>
     </div>
   );
+}
 }
 
 export default App;
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import { render, getByText } from '@testing-library/react';
+import Navbar from './Navbar';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders without crashing', () => {
+  render(<App />);
 });
+
+test('renders without crashing', () => {
+  render(<Navbar />);
+});
+
+test("Women's World Cup header is found", () => {
+  const {getByText} = render(<Navbar />);
+  getByText("Women's World Cup");
+});
+
+test("Top 100 Players header is found", () => {
+  const {getByText} = render(<App />);
+  getByText("Top 100 Players");
+});
+
+
